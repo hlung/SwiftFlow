3a5acf04699730d34a630bfbcab9b5bf3b64b4b7
Thongchai Kolyutsakul <thongchaikol@gmail.com>
2020-05-03 22:59:23 +0800

    Add a simple test

--- Tests/SwiftFlowTests/SwiftFlowTests.swift
+++ Tests/SwiftFlowTests/SwiftFlowTests.swift
@@ -2,14 +2,43 @@ import XCTest
 @testable import SwiftFlow
 
 final class SwiftFlowTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(SwiftFlow().text, "Hello, World!")
-    }
+  func testExample() {
+    let graph = Graph()
 
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+    graph.addFlow([
+      Node(.pill, title: "Start"),
+      Arrow(.down),
+      Node(.diamond, title: "Work\nsuccess?", id: "success"),
+      Arrow(.down, title: "Yes"),
+      Node(.rect, title: "Go Party!"),
+      Arrow(.down),
+      Node(.pill, title: "End", id: "end")
+    ])
+
+    graph.addFlow([
+      NodeShortcut(id: "success"),
+      Arrow(.right, title: "No"),
+      Node(.rect, title: "Cry"),
+      Arrow(.down),
+      Node(.rect, title: "Go home"),
+      Arrow(.down),
+      NodeShortcut(id: "end")
+    ])
+
+    XCTAssertEqual(graph.flows.count, 2)
+    XCTAssertEqual(graph.flows[0].count, 7)
+    XCTAssertEqual(graph.flows[1].count, 7)
+
+    let graphView = GraphView()
+    try! graphView.draw(graph)
+
+    XCTAssertEqual(graphView.subviews.count, 8)
+    XCTAssertNotNil(graphView.existingNodeView(with: "success"))
+    XCTAssertNotNil(graphView.existingNodeView(with: "end"))
+    XCTAssertNil(graphView.existingNodeView(with: "foo"))
+  }
+
+  static var allTests = [
+    ("testExample", testExample),
+  ]
 }
