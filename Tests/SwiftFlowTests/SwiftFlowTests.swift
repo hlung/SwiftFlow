a55ce307ac8ab938a23071e271da332b88f9dc92
Thongchai Kolyutsakul <thongchaikol@gmail.com>
2020-05-07 20:30:06 +0800

    Fix tests

--- Tests/SwiftFlowTests/SwiftFlowTests.swift
+++ Tests/SwiftFlowTests/SwiftFlowTests.swift
@@ -6,28 +6,33 @@ final class SwiftFlowTests: XCTestCase {
     let graph = Graph()
 
     graph.addFlow([
-      Node(.pill, title: "Start"),
+      Node("Start", shape: .pill),
       Arrow(.down),
-      Node(.diamond, title: "Work\nsuccess?", id: "success"),
+      Node("Work\nsuccess?", shape: .diamond, id: "success"), // declare id for later reference
       Arrow(.down, title: "Yes"),
-      Node(.rect, title: "Go Party!"),
+      Node("Go Party!", shape: .rect, id: "party"),
       Arrow(.down),
-      Node(.pill, title: "End", id: "end")
+      Node("End", shape: .pill, id: "end")
     ])
 
     graph.addFlow([
-      NodeShortcut(id: "success"),
-      Arrow(.right, title: "No"),
-      Node(.rect, title: "Cry"),
+      NodeShortcut(id: "success"), // refers back to the Node above
+      Arrow(.right, title: "No"), // branch out to the right side
+      Node("Cry", shape: .rect), // different color using config
       Arrow(.down),
-      Node(.rect, title: "Go home"),
-      Arrow(.down),
+      Node("Go home", shape: .rect, id: "home"),
+    ])
+
+    graph.addFlow([
+      NodeShortcut(id: "home"),
+      ArrowLoopBack(from: .bottom, to: .right),
       NodeShortcut(id: "end")
     ])
 
-    XCTAssertEqual(graph.flows.count, 2)
+    XCTAssertEqual(graph.flows.count, 3)
     XCTAssertEqual(graph.flows[0].count, 7)
-    XCTAssertEqual(graph.flows[1].count, 7)
+    XCTAssertEqual(graph.flows[1].count, 5)
+    XCTAssertEqual(graph.flows[2].count, 3)
 
     let graphView = GraphView()
     try! graphView.draw(graph)
